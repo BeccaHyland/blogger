e5560acb6ce0bbeae7cf83de9cd3a21822c8b150
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-22 10:42:22 -0600

    Add Article migration

--- spec/models/article-spec.rb
+++ spec/models/article-spec.rb
@@ -1,8 +1,11 @@
-require "rails-helper"
+require "rails_helper"
 
-describe Article, type: :model do
-  describe "Validations" do
-    it {should validate_presense_of(:title)}
-    it {should validate_presense_of(:body)}
+RSpec.describe do
+  describe Article, type: :model do
+    describe "validations" do
+      it {should validate_presence_of(:title)}
+      it {should validate_presence_of(:body)}
+    end
   end
+
 end
