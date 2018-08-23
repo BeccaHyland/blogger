e781ef215ae9e411fac0b3547af18f9f44b6f46b
BeccaHyland <rebecca.e.hyland@gmail.com>
2018-08-23 07:23:21 -0600

    Add article model with validations

--- db/migrate/20180822162657_create_articles.rb
+++ db/migrate/20180822162657_create_articles.rb
@@ -4,7 +4,7 @@ class CreateArticles < ActiveRecord::Migration[5.1]
       t.string :title
       t.text :body
 
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,11 +1,8 @@
 require "rails_helper"
 
-RSpec.describe do
-  describe Article, type: :model do
-    describe "validations" do
-      it {should validate_presence_of(:title)}
-      it {should validate_presence_of(:body)}
-    end
+describe Article, type: :model do
+  describe "Validations" do
+    it {should validate_presence_of(:title)}
+    it {should validate_presence_of(:body)}
   end
-
 end
